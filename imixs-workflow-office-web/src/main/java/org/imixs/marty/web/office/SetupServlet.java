1753cd9239042af518036e08b0786938f3661ed2
Ralph Soika <ralph.soika@imixs.com>
2012-12-19 17:18:03 +0000

    setupservlet starts now workflow scheduler

--- imixs-workflow-office-web/src/main/java/org/imixs/marty/web/office/SetupServlet.java
+++ imixs-workflow-office-web/src/main/java/org/imixs/marty/web/office/SetupServlet.java
@@ -7,11 +7,13 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 
 import org.imixs.office.ejb.security.UserGroupService;
+import org.imixs.workflow.ItemCollection;
+import org.imixs.workflow.jee.ejb.WorkflowService;
 
 /**
- * This servlet checks configuration on startup. The servlet is configured in
- * the web.xml with the option load-on-startup=1 which means that the servlet
- * init() method is triggered after deployment.
+ * This servlet checks configuration and scheduler on startup. The servlet is
+ * configured in the web.xml with the option load-on-startup=1 which means that
+ * the servlet init() method is triggered after deployment.
  * 
  * @author rsoika
  * 
@@ -23,15 +25,40 @@ public class SetupServlet extends HttpServlet {
 	private static Logger logger = Logger.getLogger("org.imixs.office");
 
 	@EJB
+	org.imixs.marty.ejb.WorkflowSchedulerService workflowSchedulerService;
+
+	@EJB
 	UserGroupService userGroupService;
 
 	@Override
 	public void init() throws ServletException {
 
 		super.init();
-
 		logger.info("Imixs Office Workflow - setup...");
-		userGroupService.initUserIDs();
+
+		// init userIDs for user db
+		try {
+			if (userGroupService != null) {
+				
+				userGroupService.initUserIDs();
+			}
+
+		} catch (Exception e) {
+			logger.warning("SetupServlet - unable to initUserIds "
+					+ e.getMessage());
+		}
+
+		// try to start the scheduler service
+		try {
+			ItemCollection configItemCollection = workflowSchedulerService
+					.findConfiguration();
+
+			if (configItemCollection.getItemValueBoolean("_enabled"))
+				workflowSchedulerService.start();
+		} catch (Exception e) {
+			logger.warning("SetupServlet - unable to start scheduler service "
+					+ e.getMessage());
+		}
 
 	}
 
