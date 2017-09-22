bb2025dfcb01532188cda60ed9380df21dc534b7
Ralph Soika <ralph.soika@imixs.com>
2017-09-22 22:07:53 +0200

    initial implementation
    issue #181

--- imixs-office-workflow-web/src/main/java/com/imixs/workflow/office/web/MinuteController.java
+++ imixs-office-workflow-web/src/main/java/com/imixs/workflow/office/web/MinuteController.java
@@ -7,9 +7,12 @@ import java.util.List;
 import java.util.logging.Logger;
 
 import javax.enterprise.context.ConversationScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 
 import org.imixs.marty.workflow.ChildWorkitemController;
+import org.imixs.marty.workflow.FormController;
+import org.imixs.marty.workflow.FormDefinition;
 import org.imixs.workflow.ItemCollection;
 import org.imixs.workflow.ItemCollectionComparator;
 import org.imixs.workflow.WorkflowKernel;
@@ -31,7 +34,15 @@ public class MinuteController extends ChildWorkitemController implements Seriali
 
 	private static Logger logger = Logger.getLogger(MinuteController.class.getName());
 
-	
+	protected FormController formController = null;
+
+	private FormDefinition formDefinition = null;;
+
+	public MinuteController() {
+		super();
+		formController = new FormController();
+	}
+
 	/**
 	 * This method overwrites the behavior of the childWorkitemController. The
 	 * method returns all minutes sorted by numSequenceNumber
@@ -47,22 +58,43 @@ public class MinuteController extends ChildWorkitemController implements Seriali
 			String uniqueIdRef = getParentWorkitem().getItemValueString(WorkflowKernel.UNIQUEID);
 			if (!uniqueIdRef.isEmpty()) {
 				String sQuery = null;
-				
-				sQuery="( (type:\"workitem\" OR type:\"childworkitem\" OR type:\"workitemarchive\" OR type:\"childworkitemarchive\") ";
-				sQuery+=" AND ($uniqueidref:\"" + uniqueIdRef + "\")) ";
-				
+
+				sQuery = "( (type:\"workitem\" OR type:\"childworkitem\" OR type:\"workitemarchive\" OR type:\"childworkitemarchive\") ";
+				sQuery += " AND ($uniqueidref:\"" + uniqueIdRef + "\")) ";
+
 				try {
-					minutes = this.getDocumentService().find(sQuery, 999,0);
+					minutes = this.getDocumentService().find(sQuery, 999, 0);
 					// sort by numsequencenumber
 					Collections.sort(minutes, new ItemCollectionComparator("numsequencenumber", true));
 				} catch (QueryException e) {
 					logger.warning("loadWorkitems - invalid query: " + e.getMessage());
 				}
-				
-			
+
 			}
 		}
 		return minutes;
+	}
+
+	public FormDefinition getFormDefinition() {
+		return formDefinition;
+	}
+
+	/**
+	 * This toggle method will either load a new child workitem or reset the current
+	 * 
+	 * @param id
+	 */
+	public void toggleWorkitem(String id) {
+		if (this.getWorkitem().getUniqueID().equals(id)) {
+			// reset
+			this.setWorkitem(null);
+		} else {
+			// load by id
+			super.load(id);
+
+			// update formDefinition
+			formDefinition = formController.computeFormDefinition(getWorkitem());
+		}
 	}
 
 }
