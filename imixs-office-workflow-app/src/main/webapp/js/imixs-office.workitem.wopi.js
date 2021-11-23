8097f84c62824e73209b5b531b24bf0d0cb2112e
Ralph Soika <ralph.soika@imixs.com>
2021-11-23 15:44:23 +0100

    implementation of modal dialog
    Issue #470

--- imixs-office-workflow-app/src/main/webapp/js/imixs-office.workitem.wopi.js
+++ imixs-office-workflow-app/src/main/webapp/js/imixs-office.workitem.wopi.js
@@ -1,17 +1,49 @@
 // Imixs-Adapters-Wopi Integration
 
 //var wopiLastWorkflowEvent; // stores the last workfow UI action button
-var wopiConfirmMessage;
-var restoreDocumentPreview=false
+var restoreDocumentPreview=false;
+var nextwopifile, nextwopiurl;
+var dialogActionConfirm="Confirm", dialogActionCancel="Cancel";
+
 /**
  * Init Method for the wop document integration
- * 
- *  - we verify if a wopo link has the tag 'data-wopi-openonload=true'. In this case we trigger the link
+ *  - register modal dialog to confirm unsaved changes
+ *  - verify if a wopi link has the tag 'data-wopi-openonload=true'. In this case we trigger the link
  *    to automatically load the wopi editor. 
  */
 $(document).ready(function() {
+	
+	// init save confirm dialog
+	$( "#wopi-save-dialog" ).dialog({
+      resizable: false,
+      autoOpen: false,
+      height: "auto",
+      width: 620,
+      modal: true,
+      buttons: [
+		{
+            text: dialogActionCancel,
+            click: function () {
+                $( this ).dialog( "close" );
+            }
+        }, {
+            text: dialogActionConfirm,
+            click: function () {
+            	// close viewer
+				$( this ).dialog( "close" );
+	   			imixsWopi.isModified=false;
+				if (nextwopiurl) {	 	
+					openWopiViewer(nextwopiurl,nextwopifile);
+				} else {
+        			closeWopiViewer();
+				}    
+            }
+        }],
+    });
+
 	// if an wopi-editor-link has the tag data-wopi-openonload=true than we click the link on load....
 	$('a[data-wopi-openonload="true"]').click();
+
 });
 
  
@@ -21,18 +53,20 @@ function openWopiViewer(url,filename, discardMessage) {
 	// do we have unsaved changes?
 	// if document was modifed without save then ask the user....
 	if (imixsWopi.isModified) {
-		if (!confirm(discardMessage)) {
-			// cancel operation!
-			return false;
-		} 
+		$("#wopi-save-dialog").dialog("open");
+		nextwopifile=filename;
+		nextwopiurl=url;		
+		return false;
 	}
-
+ 
 	// minimize Document Preview (if open )
 	if ($('.imixs-workitem-form .imixs-document').hasClass('split')) {
 		imixsOfficeWorkitem.closeDocumentPreview();
 		restoreDocumentPreview=true;
 	}
 	
+	nextwopifile=null;
+	nextwopiurl=null;
 	$('#wopi_header_filename_id').html(filename);
 	// hide the workflow form
 	$('#imixs_workitem_form_id').hide();
@@ -44,25 +78,24 @@ function openWopiViewer(url,filename, discardMessage) {
 }
 
 
-
+// This callback method is called on a workflow action
 function workitemSaveCallback(action) {
 	if (imixsWopi.isModified) {
-		return confirm(wopiConfirmMessage);
+		$("#wopi-save-dialog").dialog("open");
+		return false;
 	}
 }
 
 // close the wopi viewer
-function closeWopiViewer(confirmMessage) {
+function closeWopiViewer() {
 	
 	// if document was modifed without save then ask the user....
 	if (imixsWopi.isModified) {
-		if (!confirm(confirmMessage)) {
-			// cancel operation!
-			return false;
-		} 
-		imixsWopi.isModified=false;
+		$("#wopi-save-dialog").dialog("open");
+		return false;
 	}
-	
+	nextwopifile=null;
+	nextwopiurl=null;
 	if ($('#wopi_controlls').is(":visible")) {
 		//console.log("close ");
 		$('#wopi_controlls').hide();
