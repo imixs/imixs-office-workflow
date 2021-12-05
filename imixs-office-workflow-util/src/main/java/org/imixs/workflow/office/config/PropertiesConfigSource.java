b50c8aafc6991f5a453da85a88ff797b2af7f437
Ralph Soika <ralph.soika@imixs.com>
2021-12-05 13:08:59 +0100

    fixed
    Issue #474

--- imixs-office-workflow-util/src/main/java/org/imixs/workflow/office/config/PropertiesConfigSource.java
+++ imixs-office-workflow-util/src/main/java/org/imixs/workflow/office/config/PropertiesConfigSource.java
@@ -1,6 +1,7 @@
 package org.imixs.workflow.office.config;
 
 import java.util.Map;
+import java.util.Set;
 
 import org.eclipse.microprofile.config.spi.ConfigSource;
 
@@ -8,7 +9,8 @@ import org.eclipse.microprofile.config.spi.ConfigSource;
  * The Marty PropertiesConfigSource is a custom config source based on
  * Microprofile Config API.
  * <p>
- * The properties of this config source are loaded by the Marty PropertiesLoader EJB.
+ * The properties of this config source are loaded by the Marty PropertiesLoader
+ * EJB.
  * <p>
  * As per SPI it is necessary to register the implementation in
  * META-INF/services by adding an entry in a file called
@@ -23,31 +25,36 @@ import org.eclipse.microprofile.config.spi.ConfigSource;
  */
 public class PropertiesConfigSource implements ConfigSource {
 
-	public static final String NAME = "MartyConfigSource";
-	public static Map<String, String> properties = null; // use static to overwrite early registered properties
+    public static final String NAME = "MartyConfigSource";
+    public static Map<String, String> properties = null; // use static to overwrite early registered properties
 
-	@Override
-	public int getOrdinal() {
-		return 910;
-	}
+    @Override
+    public int getOrdinal() {
+        return 910;
+    }
 
-	@Override
-	public String getValue(String key) {
-		if (properties != null) {
-			return properties.get(key);
-		} else {
-			return null;
-		}
-	}
+    @Override
+    public String getValue(String key) {
+        if (properties != null) {
+            return properties.get(key);
+        } else {
+            return null;
+        }
+    }
 
-	@Override
-	public String getName() {
-		return NAME;
-	}
+    @Override
+    public String getName() {
+        return NAME;
+    }
 
-	@Override
-	public Map<String, String> getProperties() {
-		return properties;
-	}
+    @Override
+    public Map<String, String> getProperties() {
+        return properties;
+    }
+
+    @Override
+    public Set<String> getPropertyNames() {
+        return properties.keySet();
+    }
 
 }
