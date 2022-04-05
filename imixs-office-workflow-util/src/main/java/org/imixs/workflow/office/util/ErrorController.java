91d864ff51cfb089b94c1462ff390ab1e4c20dbf
Ralph Soika <ralph.soika@imixs.com>
2022-04-05 15:05:46 +0200

    improvements of error pages
    Issue #493

---
+++ imixs-office-workflow-util/src/main/java/org/imixs/workflow/office/util/ErrorController.java
@@ -0,0 +1,76 @@
+/*******************************************************************************
+ *  Imixs Workflow 
+ *  Copyright (C) 2001, 2011 Imixs Software Solutions GmbH,  
+ *  http://www.imixs.com
+ *  
+ *  This program is free software; you can redistribute it and/or 
+ *  modify it under the terms of the GNU General Public License 
+ *  as published by the Free Software Foundation; either version 2 
+ *  of the License, or (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful, 
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ *  General Public License for more details.
+ *  
+ *  You can receive a copy of the GNU General Public
+ *  License at http://www.gnu.org/licenses/gpl.html
+ *  
+ *  Project: 
+ *  	http://www.imixs.org
+ *  	http://java.net/projects/imixs-workflow
+ *  
+ *  Contributors:  
+ *  	Imixs Software Solutions GmbH - initial API and implementation
+ *  	Ralph Soika - Software Developer
+ *******************************************************************************/
+
+package org.imixs.workflow.office.util;
+
+import java.io.Serializable;
+import java.util.logging.Logger;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+/**
+ * The ErrorController provides methods to analyse a exception stack trace.
+ * The controller is used in the errorhandler.xhtml page to redirect the user to a corresponding error page.
+ * 
+ * @author rsoika
+ */
+@Named
+@RequestScoped
+public class ErrorController implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private static Logger logger = Logger.getLogger(ErrorController.class.getName());
+
+	public ErrorController() {
+		super();
+	}
+
+    /**
+     * Returns true if the exception is caused by a OptimisticLockException
+     * @param e
+     * @return
+     */
+    public boolean isCausedBy(Exception e, String excpetionType) {
+        logger.info("test is caused by: " + e);
+        if (e==null) {
+            return false;
+        }
+        // iterate over the full exception stack trace
+        Throwable cause = e.getCause();
+        while (cause!=null) {
+            if (cause.toString().contains(excpetionType)) {
+                logger.warning("...exception caused by " + excpetionType );
+                return true;
+            }
+            // take next cause from stack
+            cause=cause.getCause();
+        }
+        return false;
+    }
+}
