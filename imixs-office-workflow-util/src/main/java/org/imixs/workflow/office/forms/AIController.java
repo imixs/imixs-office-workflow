75afd893060ad900e7b339b0a19c13a773018c58
Ralph Soika <ralph.soika@imixs.com>
2024-10-13 11:00:24 +0200

    added imixs-ai as a dependency, refactoring
    Issue #610

--- imixs-office-workflow-util/src/main/java/org/imixs/workflow/office/forms/AIController.java
+++ imixs-office-workflow-util/src/main/java/org/imixs/workflow/office/forms/AIController.java
@@ -23,25 +23,15 @@
  *******************************************************************************/
 package org.imixs.workflow.office.forms;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.io.Serializable;
-import java.io.StringReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Base64;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
-import java.util.Map;
 import java.util.logging.Logger;
 
-import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.imixs.ai.workflow.OpenAIAPIService;
 import org.imixs.marty.profile.UserController;
 import org.imixs.workflow.FileData;
 import org.imixs.workflow.ItemCollection;
@@ -54,11 +44,7 @@ import jakarta.enterprise.context.ConversationScoped;
 import jakarta.enterprise.event.Observes;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
-import jakarta.json.Json;
 import jakarta.json.JsonObject;
-import jakarta.json.JsonObjectBuilder;
-import jakarta.json.JsonReader;
-import jakarta.json.JsonValue;
 
 /**
  * The AIController integrates the imixs-ai module
@@ -73,10 +59,13 @@ public class AIController implements Serializable {
 	public static final String ERROR_PROMPT_TEMPLATE = "ERROR_PROMPT_TEMPLATE";
 	public static final String ERROR_PROMPT_INFERENCE = "ERROR_PROMPT_INFERENCE";
 
-	public static final String LLM_SERVICE_ENDPOINT = "llm.service.endpoint";
-	public static final String ENV_LLM_SERVICE_ENDPOINT_USER = "llm.service.endpoint.user";
-	public static final String ENV_LLM_SERVICE_ENDPOINT_PASSWORD = "llm.service.endpoint.password";
-	public static final String ENV_LLM_SERVICE_ENDPOINT_TIMEOUT = "LLM_SERVICE_TIMEOUT";
+	// public static final String LLM_SERVICE_ENDPOINT = "llm.service.endpoint";
+	// public static final String ENV_LLM_SERVICE_ENDPOINT_USER =
+	// "llm.service.endpoint.user";
+	// public static final String ENV_LLM_SERVICE_ENDPOINT_PASSWORD =
+	// "llm.service.endpoint.password";
+	// public static final String ENV_LLM_SERVICE_ENDPOINT_TIMEOUT =
+	// "LLM_SERVICE_TIMEOUT";
 
 	private static final long serialVersionUID = 1L;
 	private static Logger logger = Logger.getLogger(AIController.class.getName());
@@ -92,21 +81,25 @@ public class AIController implements Serializable {
 	@Inject
 	protected UserController userController;
 
-	@Inject
-	@ConfigProperty(name = LLM_SERVICE_ENDPOINT, defaultValue = "a")
-	String serviceEndpoint;
+	// @Inject
+	// @ConfigProperty(name = LLM_SERVICE_ENDPOINT, defaultValue = "a")
+	// String serviceEndpoint;
+
+	// @Inject
+	// @ConfigProperty(name = ENV_LLM_SERVICE_ENDPOINT_USER, defaultValue = "b")
+	// String serviceEndpointUser;
+
+	// @Inject
+	// @ConfigProperty(name = ENV_LLM_SERVICE_ENDPOINT_PASSWORD, defaultValue = "c")
+	// String serviceEndpointPassword;
+
+	// @Inject
+	// @ConfigProperty(name = ENV_LLM_SERVICE_ENDPOINT_TIMEOUT, defaultValue =
+	// "120000")
+	// int serviceTimeout;
 
 	@Inject
-	@ConfigProperty(name = ENV_LLM_SERVICE_ENDPOINT_USER, defaultValue = "b")
-	String serviceEndpointUser;
-
-	@Inject
-	@ConfigProperty(name = ENV_LLM_SERVICE_ENDPOINT_PASSWORD, defaultValue = "c")
-	String serviceEndpointPassword;
-
-	@Inject
-	@ConfigProperty(name = ENV_LLM_SERVICE_ENDPOINT_TIMEOUT, defaultValue = "120000")
-	int serviceTimeout;
+	OpenAIAPIService openAIAPIService;
 
 	/**
 	 * This helper method is called during the WorkflowEvent.WORKITEM_CHANGED to
@@ -150,153 +143,36 @@ public class AIController implements Serializable {
 	 */
 	public void send() throws PluginException {
 
-		String input = workflowController.getWorkitem().getItemValueString("ai.chat.prompt");
+		ItemCollection workitem = workflowController.getWorkitem();
+		String input = workitem.getItemValueString("ai.chat.prompt");
 		logger.info("prompt...:" + input);
 
-		JsonObject jsonPrompt = buildJsonPromptObject(input, null);
-
-		String result = postPromptCompletion(serviceEndpoint, jsonPrompt);
-		logger.info("result=" + result);
-
-		processPromptResult(result, workflowController.getWorkitem());
-
+		// build context....
+		String prompt = buildContextPrompt(input);
+		JsonObject jsonPrompt = openAIAPIService.buildJsonPromptObject(prompt, null);
+		String result = openAIAPIService.postPromptCompletion(jsonPrompt, null);
+		openAIAPIService.processPromptResult(result, workflowController.getWorkitem(), null, null);
+		updateChatHistory(workitem);
 	}
 
 	/**
-	 * This method POST a given prompt to the endpoint '/completion' and returns the
-	 * predicted completion.
-	 * The method returns the response body.
+	 * This method adds the prompt result into the ai chat history.
 	 * 
-	 * The method optional test if the environment variables
-	 * LLM_SERVICE_ENDPOINT_USER and LLM_SERVICE_ENDPOINT_PASSWORD are set. In this
-	 * case a BASIC Authentication is used for the connection to the LLMService.
-	 * 
-	 * See details:
-	 * https://github.com/ggerganov/llama.cpp/blob/master/examples/server/README.md#api-endpoints
-	 * 
-	 * 
-	 * curl example:
-	 * 
-	 * curl --request POST \
-	 * --url http://localhost:8080/completion \
-	 * --header "Content-Type: application/json" \
-	 * --data '{"prompt": "Building a website can be done in 10 simple
-	 * steps:","n_predict": 128}'
-	 * 
-	 * @param xmlPromptData
 	 * @throws PluginException
 	 */
-	public String postPromptCompletion(String apiEndpoint, JsonObject jsonPromptObject)
-			throws PluginException {
-		String response = null;
-		try {
-			if (!apiEndpoint.endsWith("/")) {
-				apiEndpoint = apiEndpoint + "/";
-			}
-			URL url = new URL(apiEndpoint + "completion");
-			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-
-			conn.setConnectTimeout(serviceTimeout); // set timeout to 5 seconds
-			conn.setReadTimeout(serviceTimeout);
-			// Set Basic Authentication?
-			if (serviceEndpointUser != null && !serviceEndpointUser.isEmpty()
-					&& !serviceEndpointPassword.isEmpty()) {
-				String auth = serviceEndpointUser + ":" + serviceEndpointPassword;
-				byte[] encodedAuth = Base64.getEncoder().encode(auth.getBytes(StandardCharsets.UTF_8));
-				String authHeaderValue = "Basic " + new String(encodedAuth);
-				conn.setRequestProperty("Authorization", authHeaderValue);
-			}
-
-			// Set the appropriate HTTP method
-			conn.setRequestMethod("POST");
-			conn.setRequestProperty("Content-Type", "application/json; utf-8");
-			conn.setRequestProperty("Accept", "application/json");
-			conn.setDoOutput(true);
-
-			// Write the JSON object to the output stream
-			String jsonString = jsonPromptObject.toString();
-			logger.fine("JSON Object=" + jsonString);
-
-			try (OutputStream os = conn.getOutputStream()) {
-				byte[] input = jsonString.getBytes(StandardCharsets.UTF_8);
-				os.write(input, 0, input.length);
-			}
-
-			// Reading the response
-			int responseCode = conn.getResponseCode();
-			logger.fine("POST Response Code :: " + responseCode);
-			if (responseCode == HttpURLConnection.HTTP_OK) {
-				try (BufferedReader br = new BufferedReader(
-						new InputStreamReader(conn.getInputStream(), StandardCharsets.UTF_8))) {
-					StringBuilder responseBody = new StringBuilder();
-					String responseLine = null;
-					while ((responseLine = br.readLine()) != null) {
-						responseBody.append(responseLine.trim() + "\n");
-					}
-					response = responseBody.toString();
-					logger.fine("Response Body :: " + response);
-				}
-			} else {
-				throw new PluginException(AIController.class.getSimpleName(),
-						ERROR_PROMPT_INFERENCE, "Error during POST prompt: HTTP Result " + responseCode);
-			}
-			// Close the connection
-			conn.disconnect();
-			logger.fine("===== postPromptCompletion completed");
-			return response;
-
-		} catch (IOException e) {
-			logger.severe(e.getMessage());
-			throw new PluginException(
-					AIController.class.getSimpleName(),
-					ERROR_PROMPT_TEMPLATE,
-					"Exception during POST prompt - " + e.getClass().getName() + ": " + e.getMessage(), e);
+	public void updateChatHistory(ItemCollection workitem) throws PluginException {
+		// extract last prompt result
+		List<String> promptResults = workitem.getItemValueList(OpenAIAPIService.ITEM_AI_RESULT, String.class);
+		String promptResult = null;
+		if (!promptResults.isEmpty()) {
+			promptResult = promptResults.get(promptResults.size() - 1);
 		}
-
+		logger.info("Result=" + promptResult);
+		chatHistory.add(promptResult);
 	}
 
 	/**
-	 * This helper method builds a json prompt object including options params.
-	 * 
-	 * See details:
-	 * https://github.com/ggerganov/llama.cpp/blob/master/examples/server/README.md#api-endpoints
-	 * 
-	 * @param prompt
-	 * @param prompt_options
-	 * @return
-	 */
-	public JsonObject buildJsonPromptObject(String question, String prompt_options) {
-
-		// Create a JsonObjectBuilder instance
-		JsonObjectBuilder jsonObjectBuilder = Json.createObjectBuilder();
-
-		String contextPrompt = buildContextPrompt(question);
-		System.out.println(contextPrompt);
-		jsonObjectBuilder.add("prompt", contextPrompt);
-
-		// Do we have options?
-		if (prompt_options != null && !prompt_options.isEmpty()) {
-			// Create a JsonReader from the JSON string
-			JsonReader jsonReader = Json.createReader(new StringReader(prompt_options));
-			JsonObject parsedJsonObject = jsonReader.readObject();
-			jsonReader.close();
-			// Add each key-value pair from the parsed JsonObject to the new
-			// JsonObjectBuilder
-			for (Map.Entry<String, JsonValue> entry : parsedJsonObject.entrySet()) {
-				jsonObjectBuilder.add(entry.getKey(), entry.getValue());
-			}
-		}
-
-		// Build the JsonObject
-		JsonObject jsonObject = jsonObjectBuilder.build();
-
-		logger.fine("buildJsonPromptObject completed:");
-		logger.fine(jsonObject.toString());
-		return jsonObject;
-	}
-
-	/**
-	 * This helper method creates a complex prompt containing the chronical data
+	 * This helper method creates a complex prompt containing the chronicle data
 	 * 
 	 * The prompt finishes with the given question.
 	 * 
@@ -351,10 +227,12 @@ public class AIController implements Serializable {
 						if ("dms".equals(type)) {
 							String fileName = event.getItemValueString("name");
 							FileData fileData = workitem.getFileData(fileName);
-							String fileContent = fileData.getAttribute("text").toString();
-							if (fileContent != null && !fileContent.isEmpty()) {
-								prompt += "Neues Dokument hinzugefügt: " + fileName + "\n\n";
-								prompt += fileContent + "\n\n";
+							if (fileData != null && fileData.getAttribute("text") != null) {
+								String fileContent = fileData.getAttribute("text").toString();
+								if (fileContent != null && !fileContent.isEmpty()) {
+									prompt += "Neues Dokument hinzugefügt: " + fileName + "\n\n";
+									prompt += fileContent + "\n\n";
+								}
 							}
 						}
 
@@ -369,43 +247,4 @@ public class AIController implements Serializable {
 
 	}
 
-	/**
-	 * This method processes a OpenAI API prompt result in JSON format. The method
-	 * expects a workitem* including the item 'ai.result' providing the LLM result
-	 * string.
-	 * 
-	 * The parameter 'resultItemName' defines the item to store the result string.
-	 * This param can be empty.
-	 * 
-	 * The parameter 'mode' defines a resolver method.
-	 * 
-	 * @param workitem        - the workitem holding the last AI result (stored in a
-	 *                        value
-	 *                        list)
-	 * @param resultItemName  - the item name to store the llm text result
-	 * @param resultEventType - optional event type send to all CDI Event observers
-	 *                        for the LLMResultEvent
-	 * @throws PluginException
-	 */
-	public void processPromptResult(String completionResult, ItemCollection workitem) throws PluginException {
-
-		// We expect a OpenAI API Json Result object
-		// Extract the field 'content'
-		// Create a JsonReader from the JSON string
-		JsonReader jsonReader = Json.createReader(new StringReader(completionResult));
-		JsonObject parsedJsonObject = jsonReader.readObject();
-		jsonReader.close();
-
-		// extract content
-		String promptResult = parsedJsonObject.getString("content");
-		if (promptResult == null) {
-			throw new PluginException(AIController.class.getSimpleName(),
-					ERROR_PROMPT_INFERENCE, "Error during POST prompt - no result returned!");
-		}
-		promptResult = promptResult.trim();
-
-		logger.info("Result=" + promptResult);
-		chatHistory.add(promptResult);
-
-	}
 }
