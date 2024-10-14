77ef9d9710951e286ce0c91393922b9f1e46e50c
Ralph Soika <ralph.soika@imixs.com>
2024-10-14 14:14:34 +0200

    impl async function
    Issue #610

--- imixs-office-workflow-util/src/main/java/org/imixs/workflow/office/forms/AIController.java
+++ imixs-office-workflow-util/src/main/java/org/imixs/workflow/office/forms/AIController.java
@@ -23,12 +23,20 @@
  *******************************************************************************/
 package org.imixs.workflow.office.forms;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.Serializable;
+import java.io.StringReader;
+import java.net.HttpURLConnection;
+import java.nio.charset.StandardCharsets;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.logging.Logger;
 
 import org.imixs.ai.workflow.OpenAIAPIService;
@@ -44,14 +52,22 @@ import jakarta.enterprise.context.ConversationScoped;
 import jakarta.enterprise.event.Observes;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
+import jakarta.json.Json;
 import jakarta.json.JsonObject;
 
 /**
- * The AIController integrates the imixs-ai module
+ * The AIController integrates the imixs-ai module providing a AI Chat history.
+ * The method 'sendAsync' can be used to stream a prompt. This feature is used
+ * by the front end component 'workitem.ai.xhtml' to display the life-answering.
+ * After the answer was received completely the method automatically updates
+ * the workitem ''
+ * 
+ * The method buildContextPrompt creates a complex prompt based on a given
+ * question and the workflow information stored in the current workitem.
  * 
  * 
  * @see workitem_chronicle.xhtml
- * @author rsoika,gheinle
+ * @author rsoika
  */
 @Named("aiController")
 @ConversationScoped
@@ -59,18 +75,11 @@ public class AIController implements Serializable {
 	public static final String ERROR_PROMPT_TEMPLATE = "ERROR_PROMPT_TEMPLATE";
 	public static final String ERROR_PROMPT_INFERENCE = "ERROR_PROMPT_INFERENCE";
 
-	// public static final String LLM_SERVICE_ENDPOINT = "llm.service.endpoint";
-	// public static final String ENV_LLM_SERVICE_ENDPOINT_USER =
-	// "llm.service.endpoint.user";
-	// public static final String ENV_LLM_SERVICE_ENDPOINT_PASSWORD =
-	// "llm.service.endpoint.password";
-	// public static final String ENV_LLM_SERVICE_ENDPOINT_TIMEOUT =
-	// "LLM_SERVICE_TIMEOUT";
-
 	private static final long serialVersionUID = 1L;
 	private static Logger logger = Logger.getLogger(AIController.class.getName());
 
-	List<String> chatHistory;
+	List<String> aiChatHistory;
+	String currentStreamResult = "";
 
 	@Inject
 	protected WorkflowController workflowController;
@@ -81,39 +90,21 @@ public class AIController implements Serializable {
 	@Inject
 	protected UserController userController;
 
-	// @Inject
-	// @ConfigProperty(name = LLM_SERVICE_ENDPOINT, defaultValue = "a")
-	// String serviceEndpoint;
-
-	// @Inject
-	// @ConfigProperty(name = ENV_LLM_SERVICE_ENDPOINT_USER, defaultValue = "b")
-	// String serviceEndpointUser;
-
-	// @Inject
-	// @ConfigProperty(name = ENV_LLM_SERVICE_ENDPOINT_PASSWORD, defaultValue = "c")
-	// String serviceEndpointPassword;
-
-	// @Inject
-	// @ConfigProperty(name = ENV_LLM_SERVICE_ENDPOINT_TIMEOUT, defaultValue =
-	// "120000")
-	// int serviceTimeout;
-
 	@Inject
 	OpenAIAPIService openAIAPIService;
+
+	private CompletableFuture<Void> streamingFuture;
 
 	/**
 	 * This helper method is called during the WorkflowEvent.WORKITEM_CHANGED to
 	 * update the chronicle view for the current workitem.
 	 */
-	@SuppressWarnings("unchecked")
 	public void init() {
-		long l = System.currentTimeMillis();
-		chatHistory = new ArrayList<String>();
-
+		aiChatHistory = new ArrayList<String>();
 	}
 
-	public List<String> getChatHistory() {
-		return chatHistory;
+	public List<String> getAiChatHistory() {
+		return aiChatHistory;
 	}
 
 	/**
@@ -136,39 +127,92 @@ public class AIController implements Serializable {
 	}
 
 	/**
-	 * Sends a new prompt item
+	 * Sends an asynchronous prompt
 	 * 
 	 * 
 	 * '{"prompt": "Building a website","n_predict": 128}'
 	 */
-	public void send() throws PluginException {
-
+	public void sendAsync() throws PluginException {
 		ItemCollection workitem = workflowController.getWorkitem();
 		String input = workitem.getItemValueString("ai.chat.prompt");
 		logger.info("prompt...:" + input);
-
-		// build context....
 		String prompt = buildContextPrompt(input);
-		JsonObject jsonPrompt = openAIAPIService.buildJsonPromptObject(prompt, null);
-		String result = openAIAPIService.postPromptCompletion(jsonPrompt, null);
-		openAIAPIService.processPromptResult(result, workflowController.getWorkitem(), null, null);
-		updateChatHistory(workitem);
+		JsonObject jsonPrompt = openAIAPIService.buildJsonPromptObject(prompt, true, null);
+
+		aiChatHistory.add("Question: " + input);
+		// starting async http request...
+		streamingFuture = CompletableFuture.runAsync(() -> {
+			try {
+				streamPromptCompletion(jsonPrompt);
+			} catch (PluginException e) {
+				logger.severe("Error during streaming: " + e.getMessage());
+			}
+		});
 	}
 
 	/**
-	 * This method adds the prompt result into the ai chat history.
+	 * Stream a prompt....
 	 * 
-	 * @throws PluginException
 	 */
-	public void updateChatHistory(ItemCollection workitem) throws PluginException {
-		// extract last prompt result
-		List<String> promptResults = workitem.getItemValueList(OpenAIAPIService.ITEM_AI_RESULT, String.class);
-		String promptResult = null;
-		if (!promptResults.isEmpty()) {
-			promptResult = promptResults.get(promptResults.size() - 1);
+	private void streamPromptCompletion(JsonObject jsonPromptObject) throws PluginException {
+		try {
+
+			HttpURLConnection conn = openAIAPIService.createHttpConnection(null);
+			conn.setRequestProperty("Accept", "text/event-stream");
+
+			// Write the JSON object to the output stream
+			String jsonString = jsonPromptObject.toString();
+			logger.info("JSON Object=" + jsonString);
+
+			try (OutputStream os = conn.getOutputStream()) {
+				byte[] input = jsonString.getBytes(StandardCharsets.UTF_8);
+				os.write(input, 0, input.length);
+			}
+			currentStreamResult = "";
+			// Reading the response
+			int responseCode = conn.getResponseCode();
+			logger.fine("POST Response Code: " + responseCode);
+			if (responseCode == HttpURLConnection.HTTP_OK) {
+				try (BufferedReader br = new BufferedReader(
+						new InputStreamReader(conn.getInputStream(), StandardCharsets.UTF_8))) {
+					String line;
+					// StringBuilder fullResponse = new StringBuilder();
+					while ((line = br.readLine()) != null) {
+						if (line.startsWith("data: ")) {
+							String jsonData = line.substring(6);
+							JsonObject responseObject = Json.createReader(new StringReader(jsonData)).readObject();
+							String content = responseObject.getString("content");
+							boolean stop = responseObject.getBoolean("stop", false);
+							currentStreamResult = currentStreamResult + content;
+							logger.info("FullResponse: " + currentStreamResult);
+							if (stop) {
+								logger.info("request completed - adding answer....");
+								aiChatHistory.add("Answer: " + currentStreamResult);
+								// reset stream result!
+								currentStreamResult = "<!-- imixs.ai.stream.completed -->";
+								break;
+							}
+						}
+					}
+				}
+			} else {
+				throw new PluginException(AIController.class.getSimpleName(),
+						ERROR_PROMPT_INFERENCE, "Error during POST prompt: HTTP Result " + responseCode);
+			}
+			conn.disconnect();
+		} catch (IOException e) {
+			logger.severe(e.getMessage());
+			throw new PluginException(AIController.class.getSimpleName(), ERROR_PROMPT_TEMPLATE,
+					"Exception during POST prompt - " + e.getMessage(), e);
 		}
-		logger.info("Result=" + promptResult);
-		chatHistory.add(promptResult);
+	}
+
+	public boolean isStreamingComplete() {
+		return streamingFuture != null && streamingFuture.isDone();
+	}
+
+	public String getStreamResult() {
+		return currentStreamResult;
 	}
 
 	/**
@@ -192,7 +236,6 @@ public class AIController implements Serializable {
 		prompt += "Aktueller Status: " + workitem.getItemValueString(WorkflowKernel.WORKFLOWSTATUS) + "\n";
 
 		// chronical
-
 		List<Integer> years = chronicleController.getYears();
 		Collections.reverse(years);
 		for (int year : years) {
@@ -204,20 +247,14 @@ public class AIController implements Serializable {
 				// prompt += "\n" + year + "/" + month + "\n\n";
 				for (ChronicleEntity entry : chronicleEntries) {
 					String user = userController.getUserName(entry.getUser());
-
 					List<ItemCollection> cronicleEvents = entry.entries;
 					Collections.reverse(cronicleEvents);
-
 					for (ItemCollection event : cronicleEvents) {
-
 						// Date / User
 						prompt += dateFormat.format(entry.getDate()) + " - " + user + ": ";
-
 						String type = event.getItemValueString("type");
-
 						Date date = event.getItemValueDate("date");
 						String message = event.getItemValueString("message");
-
 						if ("comment".equals(type)) {
 							prompt += "Kommentar: " + message + "\n";
 						}
@@ -235,16 +272,13 @@ public class AIController implements Serializable {
 								}
 							}
 						}
-
 					}
-
 				}
 			}
 		}
 
 		prompt += "[/INST]</s>\n[INST] " + question + "[/INST]";
 		return prompt;
-
 	}
 
 }
