ee03f19ff97d61f954adb41983e5c89ca500ef0f
Ralph Soika <ralph.soika@imixs.com>
2025-03-02 11:30:23 +0100

    addapted new i:imixsFileUpload  (Issue #624)

--- imixs-office-workflow-util/src/main/java/org/imixs/workflow/office/config/TextBlockFileController.java
+++ imixs-office-workflow-util/src/main/java/org/imixs/workflow/office/config/TextBlockFileController.java
@@ -1,17 +1,15 @@
 package org.imixs.workflow.office.config;
 
 import java.io.Serializable;
-import java.util.List;
+
+import org.imixs.workflow.faces.data.DocumentController;
+import org.imixs.workflow.faces.data.WorkflowEvent;
+import org.imixs.workflow.faces.fileupload.FileUploadController;
 
 import jakarta.enterprise.context.ConversationScoped;
 import jakarta.enterprise.event.Observes;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
-
-import org.imixs.workflow.FileData;
-import org.imixs.workflow.faces.data.DocumentController;
-import org.imixs.workflow.faces.data.WorkflowEvent;
-import org.imixs.workflow.faces.fileupload.FileUploadController;
 
 /**
  * The TextBlockController is a DocumentController with a file updload feature
@@ -44,17 +42,17 @@ public class TextBlockFileController implements Serializable {
 
         if (WorkflowEvent.DOCUMENT_BEFORE_SAVE == documentEvent.getEventType()) {
             if ("textblock".equals(documentEvent.getWorkitem().getType())) {
-                List<FileData> fileList = fileUploadController.getAttachedFiles();
-                // if no new files were uploaded then skip update $file item...
-                if (fileList == null || fileList.isEmpty()) {
-                    // skip
-                    return;
-                } else {
-                    // add new files
-                    for (FileData filedata : fileList) {
-                        documentController.getDocument().addFileData(filedata);
-                    }
-                }
+                // List<FileData> fileList = fileUploadController.getAttachedFiles();
+                // // if no new files were uploaded then skip update $file item...
+                // if (fileList == null || fileList.isEmpty()) {
+                // // skip
+                // return;
+                // } else {
+                // // add new files
+                // for (FileData filedata : fileList) {
+                // documentController.getDocument().addFileData(filedata);
+                // }
+                // }
             }
         }
 
