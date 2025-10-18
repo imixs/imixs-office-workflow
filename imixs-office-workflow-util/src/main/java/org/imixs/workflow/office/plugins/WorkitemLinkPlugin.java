c56211eceaed81fbc496172ade82121ee8242a30
Ralph Soika <ralph.soika@imixs.com>
2025-10-18 16:27:27 +0200

    added new pluign, update docu (Issue #679)

---
+++ imixs-office-workflow-util/src/main/java/org/imixs/workflow/office/plugins/WorkitemLinkPlugin.java
@@ -0,0 +1,224 @@
+/****************************************************************************
+ * Copyright (c) 2022-2025 Imixs Software Solutions GmbH and others.
+ * https://www.imixs.com
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0
+ *
+ * This Source Code may also be made available under the terms of the
+ * GNU General Public License, version 2 or later (GPL-2.0-or-later),
+ * which is available at https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+ ****************************************************************************/
+
+package org.imixs.workflow.office.plugins;
+
+import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
+import java.util.logging.Logger;
+import java.util.regex.Pattern;
+
+import org.imixs.workflow.ItemCollection;
+import org.imixs.workflow.engine.plugins.AbstractPlugin;
+import org.imixs.workflow.exceptions.AccessDeniedException;
+import org.imixs.workflow.exceptions.ModelException;
+import org.imixs.workflow.exceptions.PluginException;
+import org.imixs.workflow.exceptions.ProcessingErrorException;
+import org.imixs.workflow.util.XMLParser;
+
+/**
+ * The Imixs Split&Join Plugin provides functionality to create and update
+ * sub-process instances from a workflow event in an origin process. It is also
+ * possible to update the origin process from the sub-process instance.
+ * 
+ * The plugin evaluates the txtactivityResult and the items with the following
+ * names:
+ * 
+ * subprocess_create = create a new subprocess assigned to the current workitem
+ * 
+ * subprocess_update = update an existing subprocess assigned to the current
+ * workitem
+ * 
+ * origin_update = update the origin process assigned to the current workitem
+ * 
+ * 
+ * A subprocess will contain the $UniqueID of the origin process stored in the
+ * property $uniqueidRef. The origin process will contain a link to the
+ * subprocess stored in the property txtworkitemRef. So both workitems are
+ * linked together.
+ * 
+ * 
+ * @author Ralph Soika
+ * @version 1.0
+ * @see http://www.imixs.org/doc/engine/plugins/splitandjoinplugin.html
+ * 
+ */
+public class WorkitemLinkPlugin extends AbstractPlugin {
+
+    public static final String INVALID_FORMAT = "INVALID_FORMAT";
+    public static final String METHOD_COPYDATA = "copydata";
+
+    private static final Logger logger = Logger.getLogger(WorkitemLinkPlugin.class.getName());
+
+    /**
+     * The method evaluates the workflow activity result for items with name:
+     * 
+     * copydata
+     * 
+     * 
+     * For each method a corresponding processing cycle will be started.
+     * 
+     * @throws @throws ProcessingErrorException @throws
+     *                 AccessDeniedException @throws
+     * 
+     */
+    @SuppressWarnings("unchecked")
+    public ItemCollection run(ItemCollection workitem, ItemCollection event)
+            throws PluginException, AccessDeniedException, ProcessingErrorException {
+        ItemCollection evalItemCollection = getWorkflowContext().evalWorkflowResult(event, "workitemlink", workitem,
+                false);
+
+        if (evalItemCollection == null) {
+            // no configuration found!
+            return workitem;
+        }
+
+        try {
+            // 1.) copy items from ref workitem
+            if (evalItemCollection.hasItem(METHOD_COPYDATA)) {
+                // extract the definitions...
+                List<String> processValueList = evalItemCollection.getItemValue(METHOD_COPYDATA);
+                copyData(processValueList, workitem);
+            }
+
+        } catch (ModelException e) {
+            throw new PluginException(e.getErrorContext(), e.getErrorCode(), e.getMessage(), e);
+
+        }
+
+        return workitem;
+    }
+
+    /**
+     * This method syncs the items from the ref workitem into this process instance
+     * 
+     * 
+     * 
+     * @param definitions
+     * @param originWorkitem
+     * @throws AccessDeniedException
+     * @throws ProcessingErrorException
+     * @throws PluginException
+     * @throws ModelException
+     */
+    protected void copyData(final List<String> definitions, final ItemCollection workitem)
+            throws AccessDeniedException, ProcessingErrorException, PluginException, ModelException {
+
+        if (definitions == null || definitions.size() == 0) {
+            // no definition found
+            return;
+        }
+
+        // we iterate over each declaration of a SUBPROCESS_CREATE item....
+        for (String processValue : definitions) {
+
+            if (processValue.trim().isEmpty()) {
+                // no definition
+                continue;
+            }
+            // evaluate the item content (XML format expected here!)
+            ItemCollection processData = XMLParser.parseItemStructure(processValue);
+            boolean debug = processData.getItemValueBoolean("debug");
+            logger.info("├── Copy Data...");
+            if (processData != null) {
+                String refItem = processData.getItemValueString("ref");
+                String refId = workitem.getItemValueString(refItem);
+
+                logger.info("│   ├── refItem: " + refItem);
+                logger.info("│   ├── refId  : " + refId);
+
+                ItemCollection refWorkitem = this.getWorkflowService().getDocumentService()
+                        .load(refId);
+                if (refWorkitem != null) {
+                    // copy items
+                    // now clone the field list...
+                    copyItemList(processData.getItemValueString("items"), refWorkitem, workitem, debug);
+                    logger.info("├── ✅ copyData successful");
+                    return;
+                } else {
+                    logger.warning("├── ⚠️ Ref Workitem not found!");
+                }
+
+            }
+        }
+
+    }
+
+    /**
+     * This Method copies the fields defined in 'items' into the targetWorkitem.
+     * Multiple values are separated with comma ','.
+     * <p>
+     * In case a item name contains '|' the target field name will become the right
+     * part of the item name.
+     * <p>
+     * Example: {@code
+     *   txttitle,txtfirstname
+     *   
+     *   txttitle|newitem1,txtfirstname|newitem2
+     *   
+     * }
+     * 
+     * <p>
+     * Optional also reg expressions are supported. In this case mapping of the item
+     * name is not supported.
+     * <p>
+     * Example: {@code
+     *   (^artikel$|^invoice$),txtTitel|txtNewTitel
+     *   
+     *   
+     * } A reg expression must be includes in brackets.
+     * 
+     */
+    protected void copyItemList(String items, ItemCollection source, ItemCollection target, boolean debug) {
+        // clone the field list...
+        StringTokenizer st = new StringTokenizer(items, ",");
+        while (st.hasMoreTokens()) {
+            String field = st.nextToken().trim();
+
+            // test if field is a reg ex
+            if (field.startsWith("(") && field.endsWith(")")) {
+                Pattern itemPattern = Pattern.compile(field);
+                Map<String, List<Object>> map = source.getAllItems();
+                for (String itemName : map.keySet()) {
+                    if (itemPattern.matcher(itemName).find()) {
+                        target.replaceItemValue(itemName, source.getItemValue(itemName));
+                        if (debug) {
+                            logger.info("│   ├── copy: " + itemName + " ...");
+                        }
+                    }
+                }
+
+            } else {
+                // default behavior without reg ex
+                int pos = field.indexOf('|');
+                if (pos > -1) {
+                    target.replaceItemValue(field.substring(pos + 1).trim(),
+                            source.getItemValue(field.substring(0, pos).trim()));
+                    if (debug) {
+                        logger.info("│   ├── copy: " + source.getItemValue(field.substring(0, pos).trim()) + " -> "
+                                + field.substring(pos + 1).trim());
+                    }
+                } else {
+                    target.replaceItemValue(field, source.getItemValue(field));
+                    if (debug) {
+                        logger.info("│   ├── copy: " + field + " ...");
+                    }
+                }
+            }
+        }
+    }
+
+}
--- src/site/markdown/forms/workitemlinking.md
+++ src/site/markdown/forms/workitemlinking.md
@@ -2,64 +2,64 @@
 
 Imixs-Office-Workflow provides a way to link workitems together:
 
-	<item name="project.ref" 
-	      type="custom"  
-	      path="workitemlink"
-	      options="$workflowgroup:Projekt"
-	      label="Project:" />
+    <item name="project.ref"
+          type="custom"
+          path="workitemlink"
+          options="$workflowgroup:Projekt"
+          label="Project:" />
 
+With the `options` tag you can specify the search filter to lookup for workitems.
 
-With the `options` tag you can specify the search filter to lookup for workitems. 
+Independent from the property
+"$UnqiueIDRef". The uniqueIDs from workitems linked to the current workitem are stored in
+the property "txtWorkitemRef".
 
-Independent from the property 
- "$UnqiueIDRef". The uniqueIDs from workitems linked to the current workitem are stored in 
- the property "txtWorkitemRef".
+The custom ui widget 'workitemlink' can be used to link a workitem with other workitems. The widget is provided as a custom ui component and can be added into a jsf page using the marty component library. See the following example:
 
-The custom ui widget 'workitemlink' can be used to link a workitem with other workitems.  The widget is provided as a custom ui component and can be added into a jsf page using the marty component library. See the following example:
-
-		<f:subview id="order">
-			<ui:include src="/pages/workitems/parts/workitemlink.xhtml">
-				<ui:param name="options" value="$workflowgroup:Order" />
-			</ui:include>
-		</f:subview>
-
+    	<f:subview id="order">
+    		<ui:include src="/pages/workitems/parts/workitemlink.xhtml">
+    			<ui:param name="options" value="$workflowgroup:Order" />
+    		</ui:include>
+    	</f:subview>
 
 The custom tag 'workitemlink' provides the following attributes:
 
- * workitem : defines the workitem the references should be added to
- 
- * filter : a custom filter for the lucene search and display existing references (reg expressions are supported here)
+- workitem : defines the workitem the references should be added to
 
- * hidereferences : default = false - true hides the reference list from the widget.
+- filter : a custom filter for the lucene search and display existing references (reg expressions are supported here)
 
+- hidereferences : default = false - true hides the reference list from the widget.
 
- 
-Note: the references displayed by the widget are bound directly to the workitem managed by 
- the workflowController bean. This is independet from the property 'workitem'
- 
+Note: the references displayed by the widget are bound directly to the workitem managed by
+the workflowController bean. This is independet from the property 'workitem'
+
 ## Display references
 
 It is also possible to display linked workitems in a form.
 
 <img src="workitemref_01.png" />
 
-Because a linked work item can be linked directly from the current workitem or indirectly, two differnt types of lookups are supported. 
+Because a linked work item can be linked directly from the current workitem or indirectly, two differnt types of lookups are supported.
 
 To show direct referred workitems use the part `workitemref` :
 
-	<item name="payment.ref" 
-      type="custom"  
+```xml
+    <item name="payment.ref"
+      type="custom"
       path="workitemref"
       options="($workflowgroup:Payment) "
       label="Payments" />
+```
 
 To display external workitems holding a link to the current workitem part `workitemref_external` can be used:
 
-	<item name="payment.ref" 
-      type="custom"  
+```xml
+    <item name="payment.ref"
+      type="custom"
       path="workitemref_external"
       options="($workflowgroup:Payment) "
       label="Payments" />
+```
 
 ### Table Layout
 
@@ -68,38 +68,80 @@ The linked workitems can also be displayed in a table layout:
  <img src="workitemref_02.png" />
 
 Use the part `workitemreftable` :
- 
-	<item name="payment.ref" 
-      type="custom"  
+
+```xml
+    <item name="payment.ref"
+      type="custom"
       path="workitemreftable"
       options="($workflowgroup:Payment) "
       label="Payments" />
+```
 
 Or to display external workitems use `workitemreftable_external`:
 
-	<item name="payment.ref" 
-      type="custom"  
+```xml
+	<item name="payment.ref"
+      type="custom"
       path="workitemreftable_external"
       options="($workflowgroup:Payment) "
       label="Payments" />
- 
- 
+```
+
+## Copy Data
+
+Optional you can copy a list of items from the linked workitem into the current workitem. Therefore use the plugin
+
+`org.imixs.workflow.office.plugins.WorkitemLinkPlugin`
+
+The plugin can be configured by the following workflow result definition:
+
+```xml
+<workitemlink name="copydata">
+	<ref>payment.ref</event>
+	<items>customer.name, customer.number</items>
+      <debug>false</debug>
+</workitemlink>
+```
+
+This definition copies the items 'customer.name' and 'customer.number' from the workitem referred by 'payment.ref' into the current workitem.
+
+To avoid name conflicts, the item name can be mapped to a different name by separating the new name with the ‘|’ char.
+
+```xml
+   <items>customer.name|contact.name</items>
+```
+
+In this example the item `customer.name` will be copied into the target workitem with the new item name `contact.name` .
+Copy Items by Regex
+
+The items tag also supports regular expression. See the following example with will copy all items starting with 'customer.':
+
+```xml
+<workitemlink name="copydata">
+	<ref>payment.ref</event>
+  <items>(^customer\.)</items>
+</workitemlink>
+```
+
+A regular expression must always be included in brackets.
+
+Note: In case of a regular expression you can not use item name mapping with the ‘|’ character.
+
+The option `<debug>true</debug>` prints details of the copy process into the server log
 
 ## Layout and CSS
+
 There are different CSS classes defined wthin the imixs-marty.css file. You can overwrite
-  the layout of the workitemlink widget.
+the layout of the workitemlink widget.
 
- * marty-workitemlink - defines the main widget container
+- marty-workitemlink - defines the main widget container
 
- * marty-workitemlink-referencebox - containing the reference workitem entries
- 
- * marty-workitemlink-referencebox-entry - a single workitem entry
+- marty-workitemlink-referencebox - containing the reference workitem entries
 
- * marty-workitemlink-inputbox - container with the input field
+- marty-workitemlink-referencebox-entry - a single workitem entry
 
- * marty-workitemlink-resultlist - the container where the suggest result is presented
+- marty-workitemlink-inputbox - container with the input field
 
- * marty-workitemlink-resultlist-entry - a single workitem entry
- 
+- marty-workitemlink-resultlist - the container where the suggest result is presented
 
- 
+- marty-workitemlink-resultlist-entry - a single workitem entry
